e3b693f67e2ee95259694cb5208d9c57bff3c60e
Typheye <78747175+typheye@users.noreply.github.com>
2026-06-01 01:02:11 +0800

    update

--- USB_DEVICE/App/usbd_desc.c
+++ USB_DEVICE/App/usbd_desc.c
@@ -64,7 +64,7 @@
 
 #define USBD_VID     1155
 #define USBD_LANGID_STRING     1033
-#define USBD_MANUFACTURER_STRING     "STMicroelectronics"
+#define USBD_MANUFACTURER_STRING     "Typheye"
 #define USBD_PID_FS     22352
 #define USBD_PRODUCT_STRING_FS     "TOS HID"
 #define USBD_CONFIGURATION_STRING_FS     "Custom HID Config"
